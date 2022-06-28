fe7f1c53fc5e6391d9b19481e0d70c67ccd075b1
Iwamgad <dagmawiharry2@gmail.com>
2022-06-28 06:58:41 -0700

    I did task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("{:d}{:d},".format(0, i), end=" ")
-    elif i >= 10 and i != 99:
-        print("{:d},".format(i), end=" ")
+    if i < 99:
+        print("{:02d},".format(i), end=" ")
     else:
-        print("{:d}".format(i), end="\n")
+        print("{:02d}".format(i), end=" ")

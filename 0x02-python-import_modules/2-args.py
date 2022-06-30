038b690dd27291dbd106d0b198dd989119e8cb66
Iwamgad <dagmawiharry2@gmail.com>
2022-06-30 06:05:43 -0700

    I edited the task 2 upto 9

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argNums = len(sys.argv) - 1
+    argNums = len(sys.argv) - 1
 
-if (argNums == 0):
-    print("{:d} arguments.".format(argNums))
-elif (argNums == 1):
-    print("{:d} argument:".format(argNums))
-    print("{:d}: {:d}".format(argNums, sys.argv[argNums]))
-else:
-    print("{:d} arguments:".format(argNums))
-    for i in range(1, argNums + 1):
-        print("{:d}: {:}".format(i, sys.argv[i]))
+    if (argNums == 0):
+        print("{:d} arguments.".format(argNums))
+    elif (argNums == 1):
+        print("{:d} argument:".format(argNums))
+        print("{:d}: {:d}".format(argNums, sys.argv[argNums]))
+    else:
+        print("{:d} arguments:".format(argNums))
+        for i in range(1, argNums + 1):
+            print("{:d}: {:}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argNums = len(sys.argv)
-sum = 0
-for i in range(1, argNums):
-    sum += int(sys.argv[i])
+    argNums = len(sys.argv)
+    sum = 0
+    for i in range(1, argNums):
+        sum += int(sys.argv[i])
 
-print("{}".format(sum))
+        print("{}".format(sum))

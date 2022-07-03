1d230b27c6b27b3f6263c976d25cdb55bf3ce73b
Iwamgad <dagmawiharry2@gmail.com>
2022-07-03 09:10:40 -0700

    I did task 7 again

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-
     if len(tuple_a) == 0:
-        return tuple_b
-    elif len(tuple_b) == 0:
-        return tuple_a
-    elif len(tuple_a) == len(tuple_b):
-        temp_tuple = tuple_a
-        new_tuple = list(temp_tuple)
-        i = 0
-        while i < len(tuple_a):
-            new_tuple[i] = tuple_a[i] + tuple_b[i]
-            i = i + 1
-        new_tuple = tuple(new_tuple)
-    else:
-        if len(tuple_a) < len(tuple_b):
-            maxIndex = len(tuple_a)
-            temp_tuple = tuple_b
-            new_tuple = list(temp_tuple)
-        else:
-            temp_tuple = tuple_a
-            new_tuple = list(temp_tuple)
-            maxIndex = len(tuple_b)
-        i = 0
-        while i < maxIndex:
-            new_tuple[i] = tuple_a[i] + tuple_b[i]
-            i = i + 1
-        new_tuple = tuple(new_tuple)
-    return new_tuple
+        tuple_a += (0, 0)
+    elif len(tuple_a) == 1:
+        tuple_a += (0,)
+    elif len(tuple_a) > 2:
+        tuple_a = tuple_a[:2]
+
+    if len(tuple_b) == 0:
+        tuple_b += (0, 0)
+    elif len(tuple_b) == 1:
+        tuple_b += (0,)
+    elif len(tuple_b) > 2:
+        tuple_b = tuple_b[:2]
+    sum = tuple(sum(item) for item in zip(tuple_a, tuple_b))
+    return sum

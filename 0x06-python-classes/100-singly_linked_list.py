e1afb8f8f86c362114cb0749af36d716373ab5f9
Iwamgad <dagmawiharry2@gmail.com>
2022-07-19 07:33:19 -0700

    I did task 7 again

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -4,92 +4,66 @@
 This module implements a basic singly linked list
 """
 
-
 class Node:
-    """The node of a singly linked list
-    """
     def __init__(self, data, next_node=None):
-        """Constructs node object.
-        Args:
-            data (int): The data of the Node.
-            next_node (:obj:node, optional): The next Node.
-        Raises:
-            TypeError: If data is not an integer.
-                If next_node is not a Node objet.
+        """Defines a node for a singly linked list
         """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """
-        Args:
-            data (int): The data of the Node.
-        Raises:
-            TypeError: If data is not an integer.
-        """
-        return self._Node__data
+        return self.__data
 
     @data.setter
-    def data(self, data):
-        if data and not isinstance(data, int):
-            raise TypeError("data must be an integer")
-        else:
-            self._Node__data = data
+    def data(self, value):
+        if type(value) != int:
+            raise TypeError('data must be an integer')
+
+        self.__data = value
 
     @property
     def next_node(self):
-        """
-        Args:
-            data (int): The data of the Node.
-        Raises:
-            TypeError: If data is not an integer.
-        """
-
-        return self._Node__next_node
+        return self.__next_node
 
     @next_node.setter
-    def next_node(self, next_node):
-        if next_node and not isinstance(next_node, Node):
-            raise TypeError("next_node must be an integer")
-        else:
-            self._Node__next_node = next_node
+    def next_node(self, value):
+        if value is not None and type(value) != Node:
+            raise TypeError('next_node must be a Node object')
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-    """Definition of a singly linked list.
-    """
     def __init__(self):
-        self.head = None
-
-    def __str__(self):
-        res = ""
-        tmp = self.head
-        while tmp.next_node is not None:
-            res += str(tmp.data)
-            tmp = tmp.next_node
-            if tmp.next_node is not None:
-                res += "\n"
-        return res
+        """Defines the singly linked list
+        """
+        self.__head = None
 
     def sorted_insert(self, value):
-        """Inserts in a sorted singly linked list
-        Args:
-            value (int): The value to insert.
-        """
-        try:
-            new_node = Node(value)
-        except Exception:
-            return
-        if not self.head:
-            self.head = new_node
+        if self.__head is None:
+            self.__head = Node(value)
         else:
-            tmp = self.head
-            while value > tmp.data and tmp.next_node is not None:
-                prev = tmp
-                tmp = tmp.next_node
-            new_node.next_node = tmp
-            if tmp == self.head:
-                self.head = new_node
+            current = self.__head
+            previous = None
+            while current and value > current.data:
+                previous = current
+                current = current.next_node
+            if current is None:
+                previous.next_node = Node(value)
+            elif current is self.__head and previous is None:
+                self.__head = Node(value, current)
             else:
-                prev.next_node = new_node
+                newNode = Node(value, current)
+                previous.next_node = newNode
+
+    def __repr__(self):
+        node = self.__head
+        text = ''
+        while 1:
+            text += str(node.data)
+            node = node.next_node
+            if node.next_node is None:
+                break
+            else:
+                text += '\n'
+        return text

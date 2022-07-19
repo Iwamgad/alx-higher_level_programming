c21a76f562ca414e1063eee0a588fc953c6ac73b
Iwamgad <dagmawiharry2@gmail.com>
2022-07-19 03:46:09 -0700

    I did task 0 again

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 """Square module definition.
 This module defines an empty `Square` class
 """
 class Square:
-    """an empty class"""
+    """An empty `Square` class
+    For now the class has neither `Atribures` nor `Methods`
+    """
     pass

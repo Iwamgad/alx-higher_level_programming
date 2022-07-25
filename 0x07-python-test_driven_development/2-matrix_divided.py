d24eedf8f5d55304c7d1b1e69a54c4962fc93b14
Iwamgad <dagmawiharry2@gmail.com>
2022-07-25 10:18:13 -0700

    I did task 2 again

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,25 +1,49 @@
 #!/usr/bin/python3
 """
-    My addition module
+    My matrix division module
 """
 
-def add_integer(a, b=98):
-    """Adds two integers
+def matrix_divided(matrix, div):
+    """Divide all element of a matrix
     Args:
-        a (int or float): First parameter
-        b (int or float): Second Parameter
-    Returns:
-        int: The sum of the two numbers
+        matrix(list): A list of lists of int/floats
+        div(int or float): The divisor
     Raises:
-        TypeError: If a or b are not integers or floats
+        TypeError:  If matrix is not a list of lists of integers or floats
+                    If each row of the matrix are not of the same size
+                    If div must be a number (integer or float)
+        ZeroDivisionError: If div is equal to 0
+    Returns:
+        matrix(list): A new matrix with all the elements divided
     """
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    if not type(a) is int:
-        raise TypeError("a must be an integer")
-    elif not type(b) is int:
-        raise TypeError("b must be an integer")
-    else:
-        return a + b
+    # Checks if the matrix is a list
+    if not type(matrix) is list:
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        "of integers/floats")
+    # Getting the length of the first list in the matrix
+    if type(matrix[0]) is list:
+        rowLength = len(matrix[0])
+    # Running through the matrix
+    for Lists in matrix:
+        # Checks if each elements of the matrix is a list
+        if not type(Lists) is list:
+            raise TypeError("matrix must be a matrix (list of lists) of "
+                            "integers/floats")
+        # Checks if the length of each row is the same
+        if len(Lists) != rowLength:
+            raise TypeError("Each row of the matrix must have the same size")
+        # Checks if the elements in each lists of the matrix are int or float
+        for elements in Lists:
+            if not type(elements) is (int or float):
+                raise TypeError("matrix must be a matrix (list of lists) of "
+                                "integers/floats")
+    # Checks if div is a number
+    if not type(div) is (int or float):
+        raise TypeError("div must be a number")
+    # Checks if div is 0
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    # Dividing each elements of the lists of the matrix and creating a new one
+    retMatrix = [[round(index / div, 2) for index in Lists] for Lists in matrix]
+
+    return retMatrix

6600a17a78727b8e160e4306bbc12e679f132930
Iwamgad <dagmawiharry2@gmail.com>
2022-10-08 09:13:22 -0700

    I edited some files

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,3 +8,9 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+class State(Base):
+    """The State representation"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)

d539bd4c4d99e357e928ae84fe1301d7a55aa233
Iwamgad <dagmawiharry2@gmail.com>
2022-10-20 09:14:08 -0700

    I did the tasks

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A Bash script that takes in a URL as an argument, sends a GET request to the URL, 
+# and displays the body of the response
+
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
